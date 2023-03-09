ffbfe4e2e67cbf455662498fdf64d92afbb654ac
mouluo14 <2300213315@qq.com>
2023-03-09 10:06:03 +0800

    first commit

---
+++ src/main/java/com/thief1/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.thief1.git;
+
+/**
+ * @author Administrator
+ * @title: GitTest
+ * @projectName git-test
+ * @description: 123
+ * @date 2023/3/9 9:58
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git !!");
+    }
+}
