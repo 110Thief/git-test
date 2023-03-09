1fc510a6651a9c30f9655a1955239408fb96c6df
110Thief <125810416+110Thief@users.noreply.github.com>
2023-03-09 14:13:55 +0800

    pull commit

--- src/main/java/com/thief1/git/GitTest.java
+++ src/main/java/com/thief1/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("master test !!");
         System.out.println("no2 test !!");
         System.out.println("push test !!");
+        System.out.println("pull test !!");
     }
 }
